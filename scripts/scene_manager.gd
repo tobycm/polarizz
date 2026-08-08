d0f074c2199ab885f30951eefc4d73bcc01a6d0a
Toby Cm <toby@vsus.app>
2026-08-08 13:27:25 -0700

    scene manager :drooling_face:
    Signed-off-by: Toby Cm <toby@vsus.app>

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,1 +1,24 @@
 extends Node
+
+const SCENES = {
+	"main_menu": "res://scenes/menus/main_menu.tscn",
+	"level_1": "res://scenes/levels/level_test.tscn",
+	"game_over": "res://scenes/menus/game_over.tscn"
+}
+
+# The public function you will call from other scripts
+func switch_to_scene(scene_alias: String) -> void:
+	if not SCENES.has(scene_alias):
+		push_error("SceneManager: Scene alias '%s' does not exist." % scene_alias)
+		return
+		
+	var scene_path = SCENES[scene_alias]
+	
+	# We use call_deferred to safely change the scene at the end of the current frame
+	# This prevents crashes if a physics callback or signal is currently running
+	call_deferred("_deferred_switch_scene", scene_path)
+
+func _deferred_switch_scene(scene_path: String) -> void:
+	var result = get_tree().change_scene_to_file(scene_path)
+	if result != OK:
+		push_error("SceneManager: Failed to load scene at path: " + scene_path)
